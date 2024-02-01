4b4ba9a5cbf2ee9cd00c561a2fe8433cd72936eb
Rayan <rchouchane@iut.univ-paris8.fr>
2024-02-01 09:39:45 +0100

    fix: maintenant le dark mode est fonctionnelle

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -1,20 +1,18 @@
-function switchTheme() {
-  console.log("dark-theme.js loaded");
+document
+  .getElementById("theme-switcher")
+  .addEventListener("click", switchTheme);
 
-  document
-    .getElementById("theme-switcher")
-    .addEventListener("click", function (event) {
-      event.stopPropagation(); // Éviter la propagation d'événements
-      var darkThemeLink = document.getElementById("dark-theme-link");
+function switchTheme(event) {
+  event.stopPropagation(); // Évite la propagation d'événements
+  var darkThemeLink = document.getElementById("dark-theme-link");
 
-      if (darkThemeLink) {
-        darkThemeLink.remove();
-      } else {
-        var link = document.createElement("link");
-        link.id = "dark-theme-link";
-        link.rel = "stylesheet";
-        link.href = "css/dark-theme.css";
-        document.head.appendChild(link);
-      }
-    });
+  if (darkThemeLink) {
+    darkThemeLink.remove();
+  } else {
+    var link = document.createElement("link");
+    link.id = "dark-theme-link";
+    link.rel = "stylesheet";
+    link.href = "css/dark-theme.css";
+    document.head.appendChild(link);
+  }
 }
